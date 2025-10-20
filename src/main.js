927a4bbcb69cc8c27bd7ebf7b546a6bc71f77461
nishinokoki <donguriguri240@gmail.com>
2025-10-20 18:18:55 +0900

    deleted all writed statement

--- src/main.js
+++ src/main.js
@@ -5,18 +5,12 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello!!!!!!!!!!!!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1>H</h1>
+    <div>
+      <button></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
+    <p>
     </p>
   </div>
 `
